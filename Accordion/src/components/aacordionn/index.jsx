2ef583a8870eac283251647651c02c9a45357d1d
Ume-ammara <aume9119@gmail.com>
2024-03-02 17:11:16 +0500

    first commit

---
+++ Accordion/src/components/aacordionn/index.jsx
@@ -0,0 +1,66 @@
+// single selection
+// multipel selection
+
+import { useState } from "react";
+import data from "../data";
+import "./styles.css";
+
+export default function Accordion() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multipel, setMultipel] = useState([]);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  function handleMultiSelection(getCurrentId) {
+    let copyMultipel = [...multipel];
+    const findIndexOfCurrentId = copyMultipel.indexOf(getCurrentId);
+
+    if (findIndexOfCurrentId === -1) copyMultipel.push(getCurrentId);
+    else copyMultipel.splice(findIndexOfCurrentId, 1);
+
+    setMultipel(copyMultipel);
+  }
+
+  return (
+    <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multipel.indexOf(dataItem) !== -1 && (
+                    <div className="content">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>
+                  )}
+              {/* {selected === dataItem.id ||
+              multipel.indexOf(dataItem.id) !== -1 ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null} */}
+            </div>
+          ))
+        ) : (
+          <div>No data found</div>
+        )}
+      </div>
+    </div>
+  );
+}
